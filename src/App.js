4c0bb4837e76102820ff7b6ccb2f4876e2b41d4c
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-04-14 12:02:33 -0500

    completed redux setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import Navigation from './components/Navigation'
-import './App.css'
-import Router from './Router'
-import { BrowserRouter } from 'react-router-dom'
+import Navigation from './components/Navigation';
+import './App.css';
+import Router from './Router';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navigation />
-      <Router />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navigation />
+        <Router />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
